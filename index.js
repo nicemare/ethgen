6eea55f6432fc9323f308f29e6362018310381f3
Allif Maula Hafsah <25836391+nicemare@users.noreply.github.com>
2023-01-26 00:04:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const ethers = require('ethers')
 const fs = require('fs');
-while(true){
-    try {
-const wallet = ethers.Wallet.createRandom()
-fs.appendFileSync('wallet.txt', `${wallet.address}|${wallet.mnemonic.phrase}|${wallet.privateKey}\n`)
-console.log(`address         : ${wallet.address} \nmnemonic phrase : ${wallet.mnemonic.phrase}`)
-        }
-    catch(e) { }
-} 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('How many wallets do you want to generate? ', (loopCount) => {
+    for (let i = 0; i < parseInt(loopCount); i++) {
+        try {
+            const wallet = ethers.Wallet.createRandom()
+            fs.appendFileSync('wallet.txt', `${wallet.address}|${wallet.mnemonic.phrase}|${wallet.privateKey}\n`)
+            console.log(`address         : ${wallet.address} \nmnemonic phrase : ${wallet.mnemonic.phrase}`)
+        } catch (e) { }
+    }
+    rl.close();
+});
