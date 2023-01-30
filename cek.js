130566a953ce22a3ea8c26d21867c17ec199c31b
Allif Maula Hafsah <25836391+nicemare@users.noreply.github.com>
2023-01-30 11:18:47 +0700

    Update cek.js

--- cek.js
+++ cek.js
@@ -2,17 +2,22 @@ const fs = require('fs');
 const ethers = require('ethers');
 
 async function checkBalance(address) {
+  const startTime = Date.now();
   const provider = new ethers.providers.InfuraProvider('homestead');
   const balance = await provider.getBalance(address);
   const etherBalance = ethers.utils.formatEther(balance);
   console.log(`Balance of ${address}: ${etherBalance} ETH`);
+  return Date.now() - startTime;
 }
 
-fs.readFile('addresses.txt', 'utf8', (err, data) => {
+fs.readFile('addresses.txt', 'utf8', async (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
   const addresses = data.split('\n').filter(address => address.length > 0);
-  addresses.forEach(address => checkBalance(address));
+  const times = await Promise.all(addresses.map(address => checkBalance(address)));
+  const totalTime = times.reduce((sum, time) => sum + time, 0);
+  console.log(`Total addresses loaded: ${addresses.length}`);
+  console.log(`Total time taken: ${totalTime}ms`);
 });
